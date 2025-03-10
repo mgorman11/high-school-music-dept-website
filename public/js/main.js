601ca585a3dc899f0ae902cf0364bb747d299c60
mgorman11 <mollygorman989@gmail.com>
2025-03-10 16:14:31 -0400

    initial code

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+// main.js - Basic JavaScript file
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Welcome to the High School Music Department website!");
+    // You can add more interactive JavaScript code here.
+  });
+  
